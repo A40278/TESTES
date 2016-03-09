3a0a98dfefdf8638d35faa188aaf821e2bdf70bb
mvcorrea <mvcorrea@gmail.com>
2016-03-09 22:43:54 +0000

    test update done

--- src/test/java/pt/isel/ls/SqlTests.java
+++ src/test/java/pt/isel/ls/SqlTests.java
@@ -1,4 +1,6 @@
 package pt.isel.ls;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import java.sql.*;
@@ -12,4 +14,25 @@ public class SqlTests {
         Student std = new Student("William",30,12345,"M");
 
     }
+
+    @Test
+    public void testUpdate() throws SQLException {
+        SQLServerDataSource dataSource = new SQLServerDataSource();
+        dataSource.setServerName(System.getenv("LS_DB_SRV"));
+        dataSource.setUser(System.getenv("LS_DB_USER"));
+        dataSource.setPassword(System.getenv("LS_DB_PW"));
+        dataSource.setDatabaseName(System.getenv("LS_DB_NAME"));
+
+        Connection con = dataSource.getConnection();
+
+        String upd = "UPDATE Students SET idade=? WHERE nome=?";
+        Student std = new Student("William",30,12345,"M");
+
+        PreparedStatement prep = con.prepareStatement(upd);
+        prep.setInt(1, std.getAge());
+        prep.setString(2, std.getName());
+        prep.executeUpdate();
+        con.close();
+    }
+
 }
