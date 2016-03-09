dc0fa870c1060a624be2ee1a29900343336f24e1
edukng623 <edujorge13@gmail.com>
2016-03-09 22:54:31 +0000

    Teste insert done also

--- src/test/java/pt/isel/ls/SqlTests.java
+++ src/test/java/pt/isel/ls/SqlTests.java
@@ -1,4 +1,6 @@
 package pt.isel.ls;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import java.sql.*;
@@ -8,8 +10,22 @@ import java.sql.*;
 public class SqlTests {
 
     @Test
-    public void testInsert(){
-        Student std = new Student("William",30,12345,"M");
+    public void testInsert() throws SQLException{
 
+        SQLServerDataSource dataSource = new SQLServerDataSource();
+        dataSource.setServerName(System.getenv("LS_DB_SRV"));
+        dataSource.setUser(System.getenv("LS_DB_USER"));
+        dataSource.setPassword(System.getenv("LS_DB_PW"));
+        dataSource.setDatabaseName(System.getenv("LS_DB_NAME"));
+        Connection con = dataSource.getConnection();
+        String str = "INSERT INTO Students VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = con.prepareStatement(str);
+        Student std = new Student("Eu2",55,(int)(Math.random()*100),"M");
+        preparedStatement.setInt(1,std.getNumber());
+        preparedStatement.setString(2, std.getName());
+        preparedStatement.setInt(3,std.getAge());
+        preparedStatement.setString(4,std.getGender());
+        preparedStatement.executeUpdate();
+        con.close();
     }
 }
